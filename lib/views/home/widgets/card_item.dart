0223b5145a9103a0ffc9d095e6b5002c8cbc931d
JacksonAguiar <jacksonwellington.sa@gmail.com>
2023-10-08 08:43:51 -0300

    s

--- lib/views/home/widgets/card_item.dart
+++ lib/views/home/widgets/card_item.dart
@@ -53,38 +53,38 @@ class CardItemWidget extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 SizedBox(
-                    height: 100,
-                    width: double.maxFinite,
-                    child: item.image_url == null ||
-                            item.image_url!.isEmpty ||
-                            !item.image_url!.contains("https")
-                        ? Center(
-                            child: SvgPicture.asset(
-                              AppImages.snacks,
-                              color: Colors.grey.shade400,
-                              // fit: BoxFit.,
-                              width: 70,
-                            ),
-                          )
-                        :
-                        // FadeInImage(
-                        //     image: NetworkImage(item.image_url!),
-                        //     placeholder: AssetImage(AppImages.snacks),
-                        //     imageErrorBuilder: (context, error, stackTrace) {
-                        //       return Image.asset(AppImages.snacks,
-                        //           fit: BoxFit.fitWidth);
-                        //     },
-                        //     fit: BoxFit.fitWidth,
-                        //   ),
-                        Container()
-                    // Image.network(
-                    //     item.image_url!,
-                    //     fit: BoxFit.cover,
-                    //     errorBuilder: (context, error, stackTrace) {
-                    //       return const SizedBox();
-                    //     },
-                    //   ),
-                    ),
+                  height: 100,
+                  width: double.maxFinite,
+                  child: item.image_url == null ||
+                          item.image_url!.isEmpty ||
+                          !item.image_url!.contains("https")
+                      ? Center(
+                          child: SvgPicture.asset(
+                            AppImages.snacks,
+                            color: Colors.grey.shade400,
+                            // fit: BoxFit.,
+                            width: 70,
+                          ),
+                        )
+                      :
+                      // FadeInImage(
+                      //     image: NetworkImage(item.image_url!),
+                      //     placeholder: AssetImage(AppImages.snacks),
+                      //     imageErrorBuilder: (context, error, stackTrace) {
+                      //       return Image.asset(AppImages.snacks,
+                      //           fit: BoxFit.fitWidth);
+                      //     },
+                      //     fit: BoxFit.fitWidth,
+                      //   ),
+                      // Container()
+                      Image.network(
+                          item.image_url!,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return const SizedBox();
+                          },
+                        ),
+                ),
                 Container(
                   height: 2,
                   color: Colors.grey,
