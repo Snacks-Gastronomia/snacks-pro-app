9e402da3f92fbb58eab601bf9ef3e3ad96702db8
nicholas <nicholasvpinheiro@gmail.com>
2023-10-13 16:53:35 -0300

    blocbuilder

--- lib/views/home/item_screen.dart
+++ lib/views/home/item_screen.dart
@@ -270,190 +270,193 @@ class _ItemScreenState extends State<ItemScreen> {
                 ),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (widget.order.item.discount! > 0)
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                              width: 280,
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Colors.black12),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  const Icon(Icons.local_offer),
-                                  Text(
-                                    "Desconto de ${widget.order.item.discount!.toInt()}% aplicado",
-                                    style: const TextStyle(color: Colors.blue),
-                                  ),
-                                ],
-                              )),
-                          TextButton(
-                              onPressed: () => context
-                                  .read<MenuCubit>()
-                                  .removeDiscount(widget.order.item),
-                              child: const Text(
-                                "Remover",
-                                style: TextStyle(color: Colors.red),
-                              ))
-                        ]),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        widget.order.item.title,
-                        style: AppTextStyles.medium(18),
-                      ),
+            BlocBuilder<MenuCubit, MenuState>(builder: (context, state) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (widget.order.item.discount! > 0)
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.access_time,
-                            size: 16,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            '${widget.order.item.time} min',
-                            style: AppTextStyles.medium(16),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Este prato serve ${widget.order.item.num_served} pessoa${widget.order.item.num_served > 1 ? "s" : ""}',
-                    style: AppTextStyles.regular(12),
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  Text(
-                    widget.order.item.description!,
-                    style: AppTextStyles.regular(15,
-                        color: const Color(0xff979797)),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  if (widget.order.item.extras.isNotEmpty)
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                                width: 280,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.black12),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    const Icon(Icons.local_offer),
+                                    Text(
+                                      "Desconto de ${widget.order.item.discount!.toInt()}% aplicado",
+                                      style:
+                                          const TextStyle(color: Colors.blue),
+                                    ),
+                                  ],
+                                )),
+                            TextButton(
+                                onPressed: () => context
+                                    .read<MenuCubit>()
+                                    .removeDiscount(widget.order.item),
+                                child: const Text(
+                                  "Remover",
+                                  style: TextStyle(color: Colors.red),
+                                ))
+                          ]),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Text(
-                          "Extras",
-                          style: AppTextStyles.semiBold(18),
+                          widget.order.item.title,
+                          style: AppTextStyles.medium(18),
                         ),
-                        if (widget.order.item.limit_extra_options != null)
-                          Column(
-                            children: [
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                'Selecione até ${widget.order.item.limit_extra_options} opções',
-                                style: AppTextStyles.light(12),
-                              ),
-                            ],
-                          ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        ListView.separated(
-                            itemCount: widget.order.item.extras.length,
-                            separatorBuilder: (context, index) =>
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemBuilder: (context, index) {
-                              var list = List.from(widget.order.item.extras);
-                              double value =
-                                  double.parse(list[index]['value'].toString());
-
-                              return BlocBuilder<ItemScreenCubit,
-                                      ItemScreenState>(
-                                  // stream: null,
-                                  builder: (context, state) {
-                                bool selected =
-                                    state.order!.extras.contains(list[index]);
-                                return Container(
-                                  // height: 50,
-                                  padding: const EdgeInsets.all(15),
-                                  decoration: BoxDecoration(
-                                      color: selected
-                                          ? Colors.black
-                                          : Colors.white,
-                                      borderRadius: BorderRadius.circular(10),
-                                      border: const Border.fromBorderSide(
-                                          BorderSide(width: 2))),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Icon(
-                                            Icons.plus_one_rounded,
-                                            color: selected
-                                                ? Colors.white
-                                                : Colors.black,
-                                          ),
-                                          const SizedBox(
-                                            width: 15,
-                                          ),
-                                          SizedBox(
-                                            width: 150,
-                                            child: Text(
-                                              list[index]["title"],
-                                              style: AppTextStyles.medium(
-                                                16,
-                                                color: selected
-                                                    ? Colors.white
-                                                    : Colors.black,
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      Text(
-                                        "+${NumberFormat.currency(locale: "pt", symbol: r"R$ ").format(value)}",
-                                        style: AppTextStyles.medium(16,
-                                            color: AppColors.highlight),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              });
-                            })
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.access_time,
+                              size: 16,
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              '${widget.order.item.time} min',
+                              style: AppTextStyles.medium(16),
+                            ),
+                          ],
+                        )
                       ],
                     ),
-                  const SizedBox(
-                    height: 20,
-                  )
-                ],
-              ),
-            )
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Este prato serve ${widget.order.item.num_served} pessoa${widget.order.item.num_served > 1 ? "s" : ""}',
+                      style: AppTextStyles.regular(12),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                      widget.order.item.description!,
+                      style: AppTextStyles.regular(15,
+                          color: const Color(0xff979797)),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    if (widget.order.item.extras.isNotEmpty)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Extras",
+                            style: AppTextStyles.semiBold(18),
+                          ),
+                          if (widget.order.item.limit_extra_options != null)
+                            Column(
+                              children: [
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  'Selecione até ${widget.order.item.limit_extra_options} opções',
+                                  style: AppTextStyles.light(12),
+                                ),
+                              ],
+                            ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          ListView.separated(
+                              itemCount: widget.order.item.extras.length,
+                              separatorBuilder: (context, index) =>
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemBuilder: (context, index) {
+                                var list = List.from(widget.order.item.extras);
+                                double value = double.parse(
+                                    list[index]['value'].toString());
+
+                                return BlocBuilder<ItemScreenCubit,
+                                        ItemScreenState>(
+                                    // stream: null,
+                                    builder: (context, state) {
+                                  bool selected =
+                                      state.order!.extras.contains(list[index]);
+                                  return Container(
+                                    // height: 50,
+                                    padding: const EdgeInsets.all(15),
+                                    decoration: BoxDecoration(
+                                        color: selected
+                                            ? Colors.black
+                                            : Colors.white,
+                                        borderRadius: BorderRadius.circular(10),
+                                        border: const Border.fromBorderSide(
+                                            BorderSide(width: 2))),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.plus_one_rounded,
+                                              color: selected
+                                                  ? Colors.white
+                                                  : Colors.black,
+                                            ),
+                                            const SizedBox(
+                                              width: 15,
+                                            ),
+                                            SizedBox(
+                                              width: 150,
+                                              child: Text(
+                                                list[index]["title"],
+                                                style: AppTextStyles.medium(
+                                                  16,
+                                                  color: selected
+                                                      ? Colors.white
+                                                      : Colors.black,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Text(
+                                          "+${NumberFormat.currency(locale: "pt", symbol: r"R$ ").format(value)}",
+                                          style: AppTextStyles.medium(16,
+                                              color: AppColors.highlight),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                });
+                              })
+                        ],
+                      ),
+                    const SizedBox(
+                      height: 20,
+                    )
+                  ],
+                ),
+              );
+            })
           ],
         ),
       ),
