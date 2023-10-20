9724703a6423a6355f95e4157a2d1dc70c5a1570
nicholas <nicholasvpinheiro@gmail.com>
2023-10-20 14:23:22 -0300

    filtrando os pedidos com os items

--- lib/views/home/widgets/dropdown_items.dart
+++ lib/views/home/widgets/dropdown_items.dart
@@ -1,45 +1,60 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:snacks_pro_app/models/order_response.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:snacks_pro_app/components/custom_circular_progress.dart';
+
+import 'package:snacks_pro_app/views/finance/state/orders/finance_orders_cubit.dart';
 
 class DropdownItems extends StatelessWidget {
-  final dropValue = ValueNotifier('');
-  List<String> items;
-
-  DropdownItems({
-    Key? key,
-    required this.items,
-  }) : super(key: key);
+  DropdownItems({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-        valueListenable: dropValue,
-        builder: (context, String value, _) {
-          return Container(
-            decoration: BoxDecoration(
-              color: Colors.grey.shade200,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-            child: DropdownButtonHideUnderline(
-              child: DropdownButton(
-                borderRadius: BorderRadius.circular(10),
-                isExpanded: true,
-                hint: const Text("Todos os itens"),
-                value: (value.isEmpty) ? null : value,
-                onChanged: (option) => dropValue.value = option.toString(),
-                items: items
-                    .map(
-                      (option) => DropdownMenuItem(
-                        value: option,
-                        child: Text(option),
-                      ),
-                    )
-                    .toList(),
-              ),
-            ),
+    return FutureBuilder(
+      future: context.read<FinanceOrdersCubit>().getItemsByRestaurantId(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CustomCircularProgress();
+        } else if (snapshot.hasError) {
+          return Text('Erro: ${snapshot.error}');
+        } else {
+          List<String> items = snapshot.data!;
+          items.insert(0, "Todos os itens");
+
+          return BlocBuilder<FinanceOrdersCubit, FinanceOrdersState>(
+            builder: (context, state) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton(
+                    borderRadius: BorderRadius.circular(10),
+                    isExpanded: true,
+                    hint: const Text("Todos os itens"),
+                    value: state.filter.isEmpty ? null : state.filter,
+                    onChanged: (option) {
+                      context
+                          .read<FinanceOrdersCubit>()
+                          .filterItems(option.toString());
+                    },
+                    items: items
+                        .map(
+                          (option) => DropdownMenuItem(
+                            value: option,
+                            child: Text(option),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+              );
+            },
           );
-        });
+        }
+      },
+    );
   }
 }
